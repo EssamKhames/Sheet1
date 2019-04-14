deb6fc4229946403bfe12087f3224cc6efef838d
EssamKhames <48357878+EssamKhames@users.noreply.github.com>
2019-04-15 01:44:33 +0200

    Add files via upload

---
+++ IPlayerFinder.java
@@ -0,0 +1,9 @@
+package eg.edu.alexu.cad.datastructure.iceHockey;
+
+public interface IPlayerFinder {
+java.awt.Point[] findPlayers (String [] photo , int team , int thershold);
+}
+
+
+
+
---
+++ main.java
@@ -0,0 +1,11 @@
+package eg.edu.alexu.cad.datastructure.iceHockey;
+
+public class main {
+ public static void main (String args []) {
+	Impor essam = new Impor();int team =3 ;int thershold = 16;
+	String[] photo = {"33JUBU33","3U3O4433","O33P44NB","PO3NSDP3","VNDSD333","OINFD33X"};
+
+	essam.findPlayers(photo, team, thershold);
+
+ }
+}
